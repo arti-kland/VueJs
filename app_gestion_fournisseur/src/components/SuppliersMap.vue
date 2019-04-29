51e7d9713858725c590e99a783a34fc1205f572d
jpmauviel <jean-philippe.mauviel@le-campus-numerique.fr>
2019-04-29 16:56:46 +0200

    etape 7.2 de l'exo 4 avec ajout de vue-google-maps

--- app_gestion_fournisseur/src/components/SuppliersMap.vue
+++ app_gestion_fournisseur/src/components/SuppliersMap.vue
@@ -2,7 +2,23 @@
 
     <div>
         <h1>Carte des fournisseurs</h1>
-
+        <div class="container">
+            <GmapMap
+                    class="sizeMap"
+                    :center="{lat:10, lng:10}"
+                    :zoom="7"
+                    map-type-id="terrain"
+            >
+                <GmapMarker
+                        :key="index"
+                        v-for="(m, index) in markers"
+                        :position="m.position"
+                        :clickable="true"
+                        :draggable="true"
+                        @click="center=m.position"
+                />
+            </GmapMap>
+        </div>
     </div>
 </template>
 
@@ -16,5 +32,15 @@
 </script>
 
 <style scoped>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .sizeMap {
+        width: 800px;
+        height: 600px;
+    }
 
 </style>
--- app_gestion_fournisseur/src/main.js
+++ app_gestion_fournisseur/src/main.js
@@ -4,9 +4,16 @@ import App from './App.vue'
 import Suppliers from './components/SuppliersList.vue'
 import SuppliersMap from './components/SuppliersMap.vue'
 import Supplier from './components/Supplier.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 // import HelloWorld from './components/HelloWorld.vue'
 Vue.use(VueRouter)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyB_rUFcZii_mHfRgddtI0e_lgK6cQj7fG4',
+     }
+})
+
 Vue.config.productionTip = true
 
 const router = new VueRouter({
