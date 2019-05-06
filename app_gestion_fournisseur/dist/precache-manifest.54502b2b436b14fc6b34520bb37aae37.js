78645e60f64cb8811e153d4d7ead7e6cc54fed12
jpmauviel <jean-philippe.mauviel@le-campus-numerique.fr>
2019-05-06 14:43:39 +0200

    modification gitignore

---
+++ app_gestion_fournisseur/dist/precache-manifest.54502b2b436b14fc6b34520bb37aae37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "57512204d61060d1c313",
+    "url": "/js/chunk-vendors.f4fedc17.js"
+  },
+  {
+    "revision": "cfa6a98c9dce1e48f603",
+    "url": "/js/app.df0a9b41.js"
+  },
+  {
+    "revision": "ce273efdde2191878c22eb203f9be0b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "57512204d61060d1c313",
+    "url": "/css/chunk-vendors.188e04d6.css"
+  },
+  {
+    "revision": "cfa6a98c9dce1e48f603",
+    "url": "/css/app.54b3e32a.css"
+  }
+];
